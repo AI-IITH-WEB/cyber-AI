b36a75e2da1d2da7bf051fd32fda33e6e19bb3f6
Vilekya <k.vilekhya@gmail.com>
2025-04-10 14:29:06 +0530

    Added Thank you page

--- nav.js
+++ nav.js
@@ -26,12 +26,16 @@ function updateNavLinks() {
 }
 
 function hideApplyButton() {
-    if (window.location.pathname.includes("register.html")) {
-        setTimeout(() => {
-            const applyButton = document.querySelector(".contact-btn");
-            if (applyButton) {
-                applyButton.style.visibility = "hidden";
-            }
-        }, 200); 
+    if (
+      window.location.pathname.includes("register.html") ||
+      window.location.pathname.includes("thankyou.html")
+    ) {
+      setTimeout(() => {
+        const applyButton = document.querySelector(".contact-btn");
+        if (applyButton) {
+          applyButton.style.visibility = "hidden";
+        }
+      }, 200);
     }
-}
+  }
+  
